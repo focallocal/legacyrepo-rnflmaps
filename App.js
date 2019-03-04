76093a35ae6aa4b96238470478d2c5bfc0b77c36
sean <seanowenhayes@gmail.com>
2019-03-04 21:48:42 +0100

    Copied and mmodified readme from fl-maps.

--- App.js
+++ App.js
@@ -1,32 +1,21 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- * @lint-ignore-every XPLATJSCOPYRIGHT1
- */
+import React, {Component} from 'react'
+import {StyleSheet, View} from 'react-native'
+import Map from './screens/map'
+import Home from './screens/home'
+import Tabs from 'react-native-tabs'
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
 
 type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <Tabs selected="Map">
+          <Map name="Map"/>
+          <Home name="Home"/>
+        </Tabs>
       </View>
-    );
+    )
   }
 }
 
@@ -37,14 +26,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+})
---
+++ screens/home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {View, Text} from 'react-native'
+
+const Home = () => {
+  return (
+    <View>
+      <Text>HOME SWEET HOME!</Text>
+    </View>
+  )
+}
+
+export default Home
