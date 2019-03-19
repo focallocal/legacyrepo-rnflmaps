9ff12ac0f4c1e4a48910cb2f38d0c33f45fa2789
sean <seanowenhayes@gmail.com>
2019-03-19 22:08:24 +0100

    Get events and draw them on the map.

--- App.js
+++ App.js
@@ -2,11 +2,17 @@
 import  * as React from 'react'
 import { createBottomTabNavigator, createAppContainer } from 'react-navigation'
 import {Provider} from 'react-redux'
+import Meteor from 'react-native-meteor'
 import Map from './src/screens/map'
 import HomeScreen from './src/screens/home/'
 import {getStore} from './src/services/redux'
+import {getConfig} from './src/services/config'
+
+const {serverUrl} = getConfig()
 
 const store = getStore()
+
+Meteor.connect(serverUrl)
 
 const TabNavigator = createBottomTabNavigator({
   Home: HomeScreen,
